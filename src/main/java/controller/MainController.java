bf8dffb20fe31670eded2d5613325a5cbb361005
mbalcer <mateusz75319@gmail.com>
2019-05-07 19:07:52 +0200

    Added controller

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,75 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private ComboBox<?> cbClassName;
+
+    @FXML
+    private TableView<?> tableField;
+
+    @FXML
+    private TableColumn<?, ?> tcNo;
+
+    @FXML
+    private TableColumn<?, ?> tcField;
+
+    @FXML
+    private TableColumn<?, ?> tcValue;
+
+    @FXML
+    private ComboBox<?> cbObject;
+
+    @FXML
+    private Button btnCreateObject;
+
+    @FXML
+    private Button btnDeleteObject;
+
+    @FXML
+    private ComboBox<?> cbField;
+
+    @FXML
+    private TextField tfValue;
+
+    @FXML
+    private Button btnSetValue;
+
+    @FXML
+    void chooseClass() {
+
+    }
+
+    @FXML
+    void chooseField() {
+
+    }
+
+    @FXML
+    void chooseObject() {
+
+    }
+
+    @FXML
+    void createNewObject() {
+
+    }
+
+    @FXML
+    void deleteObject() {
+
+    }
+
+    @FXML
+    void setValueToField() {
+
+    }
+
+}
